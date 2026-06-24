0ae2d3e0d35ff13c68c44916532e6ef912786924
tclog41 <taigcornish1@gmail.com>
2026-06-24 17:16:04 +0100

    Update conflicts.js

--- engine/conflicts.js
+++ engine/conflicts.js
@@ -1,26 +1,22 @@
 const conflicts = {
-
     sprint_system: ["slow_walk_system"],
     crouch_system: ["speed_boost_overwrite"],
     stamina_system: ["infinite_stamina"]
-
 };
 
-function detectConflicts(selectedComponents) {
+function detectConflicts(list) {
 
     const found = [];
 
-    for (const comp of selectedComponents) {
-
-        const bad = conflicts[comp];
+    for (const item of list) {
+        const bad = conflicts[item];
         if (!bad) continue;
 
-        for (const conflict of bad) {
-            if (selectedComponents.includes(conflict)) {
+        for (const c of bad) {
+            if (list.includes(c)) {
                 found.push({
-                    type: "CONFLICT",
-                    a: comp,
-                    b: conflict
+                    a: item,
+                    b: c
                 });
             }
         }
